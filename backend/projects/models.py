b630bb825273feb32b79e66cc13125c5b6e6eb8e
kagunecode <cesar.acb98@gmail.com>
2024-03-11 16:44:13 -0500

    Created models for tasks and projects

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=30)
+    favourite = models.BooleanField(default=False)
--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from ..projects.models import Projects
 
 # Create your models here.
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+    project = models.ForeignKey(Projects,default=None,on_delete=models.CASCADE)

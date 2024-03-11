8ed5aa5c24b10860fc6903313d2b8bd2291e7b51
kagunecode <cesar.acb98@gmail.com>
2024-03-11 16:58:04 -0500

    Added views for both apps

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProjectsSerializer
+from .models import Projects
 
 # Create your views here.
+
+class ProjectsView(viewsets.ModelViewSet):
+    serializer_class = ProjectsSerializer
+    fields = Projects.objects.all()

cc572c0fece98927ba1887af500ccdaf2179c0de
kagunecode <cesar.acb98@gmail.com>
2024-03-13 10:48:40 -0500

    Fixed typo

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Projects(models.Model):
     name = models.CharField(max_length=100)
-    color = models.CharField(max_length=30)
+    colour = models.CharField(max_length=30)
     favourite = models.BooleanField(default=False)
 
     def __str__(self):

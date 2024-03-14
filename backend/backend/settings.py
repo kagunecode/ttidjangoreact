84b96d93b120880c776fe18c48f650cb451f8c5d
kagunecode <cesar.acb98@gmail.com>
2024-03-14 12:49:56 -0500

    Allowed CORS credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,5 +138,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000", "http://127.0.0.1:5173", "http://localhost:5173"]
+CORS_ALLOW_CREDENTIALS = True
 
 AUTH_USER_MODEL = 'user.User'

4c321721128d34ae2fcf1000a6afb997976d4de6
kagunecode <cesar.acb98@gmail.com>
2024-03-14 16:31:38 -0500

    Retrieved user info and stored in state

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function Home() {
-  return <h1>Home</h1>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get("http://127.0.0.1:8000/api/user");
+      setData(data);
+    };
+
+    getData();
+  }, []);
+  return <h1>{data.name}</h1>;
 }

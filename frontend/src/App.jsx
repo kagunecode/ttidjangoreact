7a0924a40757682fff9236bb5ca1ea92bc3e7e3c
kagunecode <cesar.acb98@gmail.com>
2024-03-14 14:50:49 -0500

    Added project routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
 import { Home } from "./pages/Home";
+import { SignUp } from "./pages/SingUp";
+import { Login } from "./pages/Login";
+import { Navbar } from "./components/Nabvar";
 
 export default function App() {
   return (
     <>
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path="/" Component={Home} index />
+        <Route path="/signup" Component={SignUp} />
+        <Route path="/login" Component={Login} />
+      </Routes>
     </>
   );
 }

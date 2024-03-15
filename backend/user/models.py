bf66f375cbd8bdb1cb28cd3546631e177510ed60
kagunecode <cesar.acb98@gmail.com>
2024-03-15 16:51:59 -0500

    Removed comments

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)

6d4ba66ae2be009bee52f9b10b55de03147f5614
kagunecode <cesar.acb98@gmail.com>
2024-03-16 12:49:48 -0500

    Added tokens to local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,13 @@
 import { create } from "zustand";
+import { jwtDecode } from "jwt-decode";
 
 export const useStore = create((set) => ({
-  tokenData: "",
+  tokenData: localStorage.getItem("authTokens")
+    ? jwtDecode(JSON.parse(localStorage.getItem("authTokens")).access)
+    : "",
+  localToken: localStorage.getItem("authTokens")
+    ? JSON.parse(localStorage.getItem("authTokens"))
+    : null,
   setTokenData: (data) => set(() => ({ tokenData: data })),
+  setLocalToken: (data) => set(() => ({ localToken: data })),
 }));

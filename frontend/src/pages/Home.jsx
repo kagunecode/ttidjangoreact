4ee5954cdb50c803b518810c4d8fb7442120f31f
kagunecode <cesar.acb98@gmail.com>
2024-03-18 12:11:33 -0500

    Added project call to get current user projects

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import { useStore } from "../store";
+import axios from "axios";
 
 export function Home() {
+  const [projects, setProjects] = useState([]);
   const [tokenData, localToken] = useStore((state) => [
     state.tokenData,
     state.localToken,
   ]);
-  console.log(tokenData);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("http://127.0.0.1:8000/projects/", {
+        headers: { Authorization: `Bearer ${localToken.access}` },
+      });
+      setProjects(response.data);
+    })();
+  }, []);
+
+  const projectRender = projects.map((project) => (
+    <h1 key={project.id} style={{ background: project.colour }}>
+      {project.name}
+    </h1>
+  ));
   return (
     <div>
       {localToken && <h1 className="text-3xl">Welcome {tokenData.name}</h1>}
+      <h1 className="text-7xl">Your Projects</h1>
+      {projectRender}
     </div>
   );
 }

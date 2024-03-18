0995619c046d8421f659a4274b8e286848743905
kagunecode <cesar.acb98@gmail.com>
2024-03-18 12:10:25 -0500

    Added protected get route for getting projects

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,9 +1,29 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import ProjectsSerializer
 from .models import Projects
 
 # Create your views here.
 
-class ProjectsView(viewsets.ModelViewSet):
-    serializer_class = ProjectsSerializer
-    queryset = Projects.objects.all()
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getProjects(request):
+    user, _ = JWTAuthentication().authenticate(request)
+    email = user.email
+    projects = Projects.objects.filter(user__email=email)
+    serializer = ProjectsSerializer(projects, many=True)
+    return Response(serializer.data)
+    
+
+
+
+# @permission_classes([IsAuthenticated])
+# class ProjectsView(viewsets.ModelViewSet):
+#     serializer_class = ProjectsSerializer
+
+#     def list(self, request):
+#         print(self.request.headers.authorization)
+#         return Response('Ok')

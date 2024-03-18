d9f84ddc8485f5bb825b4f27ca1ae82c5e6711fc
kagunecode <cesar.acb98@gmail.com>
2024-03-17 22:27:30 -0500

    Added logout render condition

--- frontend/src/components/Nabvar.jsx
+++ frontend/src/components/Nabvar.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import useStore from "../store";
 
 export function Navbar() {
+  const localToken = useStore((state) => state.localToken);
   return (
     <nav className="flex gap-5">
       <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Signup</Link>
+      {localToken && <Link to="/logout">Login</Link>}
+      {!localToken && (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </>
+      )}
     </nav>
   );
 }

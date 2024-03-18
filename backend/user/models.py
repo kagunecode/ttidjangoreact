aadb71369ef406c9a99d30fce512cfc7aec3356b
kagunecode <cesar.acb98@gmail.com>
2024-03-18 12:10:38 -0500

    Fixed user creation methods

--- backend/user/models.py
+++ backend/user/models.py
@@ -5,7 +5,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None
+    username = models.CharField(max_length=255, null=True, default=None)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']

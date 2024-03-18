2132df20a45cff96246d59c32d52c4e7504b2517
kagunecode <cesar.acb98@gmail.com>
2024-03-18 12:11:20 -0500

    Added conditional rendering for login and logout

--- frontend/src/components/Nabvar.jsx
+++ frontend/src/components/Nabvar.jsx
@@ -1,12 +1,27 @@
-import { Link } from "react-router-dom";
-import useStore from "../store";
+import { Link, useNavigate } from "react-router-dom";
+import { useStore } from "../store";
 
 export function Navbar() {
+  const navigate = useNavigate();
   const localToken = useStore((state) => state.localToken);
+  const [setTokenData, setLocalToken] = useStore((state) => [
+    state.setTokenData,
+    state.setLocalToken,
+  ]);
+  const handleLogout = () => {
+    localStorage.removeItem("authTokens");
+    setTokenData("");
+    setLocalToken(null);
+    navigate("/login");
+  };
   return (
     <nav className="flex gap-5">
       <Link to="/">Home</Link>
-      {localToken && <Link to="/logout">Login</Link>}
+      {localToken && (
+        <a className="cursor-pointer" onClick={handleLogout}>
+          Logout
+        </a>
+      )}
       {!localToken && (
         <>
           <Link to="/login">Login</Link>

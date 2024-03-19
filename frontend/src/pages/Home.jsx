d2de0185cc275927c21a8022ba70ceb5760b5017
kagunecode <cesar.acb98@gmail.com>
2024-03-19 11:54:06 -0500

    Moved user data info

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,39 +1,19 @@
 import { useEffect } from "react";
 import { useStore } from "../store";
 import { instance } from "../utils/axiosInstance";
+import { Sidebar } from "../components/Sidebar";
+import { Navbar } from "../components/Nabvar";
 
 // TODO: Reset headers since a long exp token will persist until reload or until the time has passed
 
 export function Home() {
-  const [user, setUserData] = useStore((state) => [
-    state.userData,
-    state.setUserData,
-  ]);
-  const [tokenData, localToken] = useStore((state) => [
-    state.tokenData,
-    state.localToken,
-  ]);
-
-  useEffect(() => {
-    getUserData();
-  }, []);
-
-  const getUserData = async () => {
-    let response = await instance.get("projects/");
-    console.log(response);
-    setUserData(response.data);
-  };
-
-  const projectRender = user.map((project) => (
-    <h1 key={project.id} style={{ background: project.colour }}>
-      {project.name}
-    </h1>
-  ));
   return (
-    <div>
-      {localToken && <h1 className="text-3xl">Welcome {tokenData.name}</h1>}
-      <h1 className="text-7xl">Your Projects</h1>
-      {projectRender}
+    <div className="h-full flex">
+      <Sidebar />
+      <div className="w-full">
+        <Navbar />
+        <h1 className="text-7xl">Tasks</h1>
+      </div>
     </div>
   );
 }

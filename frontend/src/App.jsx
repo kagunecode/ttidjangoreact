d0dd5aa05b6f553e0232d4cd1a39a8d4012403e1
kagunecode <cesar.acb98@gmail.com>
2024-03-19 11:53:01 -0500

    Added Welcome path for future implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { SignUp } from "./pages/SingUp";
 import { Login } from "./pages/Login";
-import { Navbar } from "./components/Nabvar";
+import { Welcome } from "./pages/Welcome";
 
 export default function App() {
   return (
-    <>
-      <Navbar />
+    <div className="flex flex-col h-full w-full">
       <Routes>
-        <Route path="/" Component={Home} index />
+        <Route path="/" Component={Welcome} index />
         <Route path="/signup" Component={SignUp} />
         <Route path="/login" Component={Login} />
+        <Route path="/app" Component={Home} />
       </Routes>
-    </>
+    </div>
   );
 }

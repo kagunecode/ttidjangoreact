e092152008e2c0ded1dd15d3432e4842ad6f1707
kagunecode <cesar.acb98@gmail.com>
2024-03-19 11:53:28 -0500

    Removed logout from Navbar tools

--- frontend/src/components/Nabvar.jsx
+++ frontend/src/components/Nabvar.jsx
@@ -1,29 +1,11 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useStore } from "../store";
 
 export function Navbar() {
-  const navigate = useNavigate();
   const localToken = useStore((state) => state.localToken);
-  const [setTokenData, setLocalToken, setUserData] = useStore((state) => [
-    state.setTokenData,
-    state.setLocalToken,
-    state.setUserData,
-  ]);
-  const handleLogout = () => {
-    localStorage.removeItem("authTokens");
-    setTokenData("");
-    setLocalToken(null);
-    setUserData([]);
-    navigate("/login");
-  };
   return (
-    <nav className="flex gap-5">
-      <Link to="/">Home</Link>
-      {localToken && (
-        <a className="cursor-pointer" onClick={handleLogout}>
-          Logout
-        </a>
-      )}
+    <nav className="flex gap-5 h-10 justify-end text-gray-400 px-4 items-center">
+      {localToken && <Link to="/">Home</Link>}
       {!localToken && (
         <>
           <Link to="/login">Login</Link>

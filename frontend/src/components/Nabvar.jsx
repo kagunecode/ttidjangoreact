0e36c6e08203a10949e92c76812b1da4b323d5ad
kagunecode <cesar.acb98@gmail.com>
2024-03-20 14:05:00 -0500

    Fixed redirection to homepage

--- frontend/src/components/Nabvar.jsx
+++ frontend/src/components/Nabvar.jsx
@@ -5,7 +5,7 @@ export function Navbar() {
   const localToken = useStore((state) => state.localToken);
   return (
     <nav className="flex gap-5 h-10 justify-end text-gray-400 px-4 items-center">
-      {localToken && <Link to="/">Home</Link>}
+      {localToken && <Link to="/app">Home</Link>}
       {!localToken && (
         <>
           <Link to="/login">Login</Link>

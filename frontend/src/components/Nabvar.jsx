f7d78ae7c5f09c8e2d06604680d4979b6e9a83a8
kagunecode <cesar.acb98@gmail.com>
2024-03-20 15:12:54 -0500

    Deleted conditions for localToken

--- frontend/src/components/Nabvar.jsx
+++ frontend/src/components/Nabvar.jsx
@@ -1,17 +1,9 @@
 import { Link } from "react-router-dom";
-import { useStore } from "../store";
 
 export function Navbar() {
-  const localToken = useStore((state) => state.localToken);
   return (
     <nav className="flex gap-5 h-10 justify-end text-gray-400 px-4 items-center">
-      {localToken && <Link to="/app">Home</Link>}
-      {!localToken && (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </>
-      )}
+      <Link to="/app">Home</Link>
     </nav>
   );
 }

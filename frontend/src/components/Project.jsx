7736bbf3ad95e4c6b410f02e85b95a13c7cdcc0c
kagunecode <cesar.acb98@gmail.com>
2024-03-20 14:05:13 -0500

    Added section for showing projects in the future

---
+++ frontend/src/components/Project.jsx
@@ -0,0 +1,19 @@
+import { instance } from "../utils/axiosInstance";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export function Project() {
+  const [projectData, setProjectData] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    (async () => {
+      const response = await instance.get(`projects/${id}/`);
+      setProjectData(response.data);
+    })();
+  }, [id]);
+  return (
+    <div className="flex flex-col h-full items-center">
+      <h1 className="text-7xl font-semibold">{projectData?.name}</h1>
+    </div>
+  );
+}

af7a0d1791a1ae443ecd34cce3486ebfa06f57e4
kagunecode <cesar.acb98@gmail.com>
2024-03-20 14:06:27 -0500

    Added styling for signup page

--- frontend/src/pages/SingUp.jsx
+++ frontend/src/pages/SingUp.jsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { IconTodoers } from "../icons/IconTodoers";
+import { Link } from "react-router-dom";
 
 export function SignUp() {
   const { register, handleSubmit } = useForm();
@@ -15,34 +17,51 @@ export function SignUp() {
     navigate("/login");
   };
   return (
-    <section className="h-full w-full flex items-center justify-center">
+    <section className="h-full w-full flex flex-col gap-10 items-center justify-center">
+      <Link to="/" className="flex gap-4 items-center">
+        <IconTodoers />
+        <h1 className="text-7xl">Todoers</h1>
+      </Link>
+      <h2 className="text-3xl text-gray-600">
+        Register and start origanizing your time
+      </h2>
       <form
         className="flex w-[30%] flex-col gap-5"
         onSubmit={handleSubmit(onSubmit)}
       >
         <input
-          className="border p-2"
+          className="border p-2 rounded"
           {...register("name")}
           type="text"
           placeholder="Username"
         />
         <input
-          className="border p-2"
+          className="border p-2 rounded"
           {...register("email")}
           type="email"
           placeholder="Email"
         />
         <input
-          className="border p-2"
+          className="border p-2 rounded"
           {...register("password")}
           type="password"
           placeholder="Password"
         />
         <input
-          className="bg-blue-500 cursor-pointer hover:bg-blue-700 duration-150 text-white p-4"
+          className="bg-orange-300 cursor-pointer hover:bg-orange-400 duration-150 text-white p-3 rounded"
           type="submit"
+          value="Register"
         />
       </form>
+      <h1>
+        Already have an account?{" "}
+        <Link
+          to="/login"
+          className="text-orange-300 hover:text-orange-400 duration-150 font-bold"
+        >
+          Login now
+        </Link>
+      </h1>
     </section>
   );
 }

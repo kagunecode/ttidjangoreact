713e616322f0e36820ee39d4d5f680ebb4592255
kagunecode <cesar.acb98@gmail.com>
2024-03-21 10:58:16 -0500

    Deteled TODO note

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import { Project } from "../components/Project";
 import { AddProject } from "../components/AddProject";
 
-// TODO: Reset headers since a long exp token will persist until reload or until the time has passed
-
 export function Home() {
   return (
     <div className="h-full relative flex">

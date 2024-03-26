d5f6a6451d7f2b875b2752d96f0bec15f73ad7a6
kagunecode <cesar.acb98@gmail.com>
2024-03-25 21:05:24 -0500

    Changed plus icon

--- frontend/src/icons/IconPlus.jsx
+++ frontend/src/icons/IconPlus.jsx
@@ -10,7 +10,7 @@ export const IconPlus = () => (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      d="M12 4.5v15m7.5-7.5h-15"
     />
   </svg>
 );

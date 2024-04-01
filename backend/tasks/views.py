1b039dae8b792d06f3f4b9fb5e19817a3c71a518
kagunecode <cesar.acb98@gmail.com>
2024-04-01 10:27:44 -0500

    Added get and post request for tasks

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,9 +1,24 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import TasksSerializer
 from .models import Tasks
 
 # Create your views here.
 
-class TasksView(viewsets.ModelViewSet):
+class TasksView(APIView):
+    #permission_classes = [IsAuthenticated]
     serializer_class = TasksSerializer
-    queryset = Tasks.objects.all()
+    
+    def get(self, request, project_id):
+        user = request.user
+        tasks = Tasks.objects.filter(user__email=user, project=project_id)
+        serializer = self.serializer_class(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

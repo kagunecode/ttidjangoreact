2e1b14247d2e19d51f20515b0ee0ded43d8db0ce
kagunecode <cesar.acb98@gmail.com>
2024-04-01 11:53:23 -0500

    Added tasks url for get and post

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('tasks/', views.TasksView.as_view())
+    path('tasks/', views.TaskView.as_view()),
+    path('tasks/<int:project_id>/', views.TasksView.as_view())
 ]

10e9efc7e2f15d81aa9f40b9be768fa38122a7a9
kagunecode <cesar.acb98@gmail.com>
2024-04-01 11:53:34 -0500

    Added new view class for general tasks

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -7,6 +7,14 @@ from .models import Tasks
 
 # Create your views here.
 
+class TaskView(APIView):
+    serializer_class = TasksSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class TasksView(APIView):
     #permission_classes = [IsAuthenticated]
     serializer_class = TasksSerializer
@@ -16,9 +24,3 @@ class TasksView(APIView):
         tasks = Tasks.objects.filter(user__email=user, project=project_id)
         serializer = self.serializer_class(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)

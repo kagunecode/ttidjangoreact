a6a3c34ac38164cb9612b27a835da433bd56006a
kagunecode <cesar.acb98@gmail.com>
2024-04-03 11:46:32 -0500

    Added task card skeleton

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,7 +1,21 @@
+import { IconPen } from "../icons/IconPen";
+import { IconTrash } from "../icons/IconTrash";
+
 export function TaskCard({ task }) {
   return (
-    <article className="bg-zinc-200 w-full">
-      <h1 key={task.id}>{task.name}</h1>
+    <article className="h-20 border-b border-gray-600 flex justify-between p-4 items-center w-full">
+      <div className="flex items-center gap-4">
+        <button className="w-6 h-6 border border-black rounded-full"></button>
+        <div>
+          <h1 className="font-bold">{task.name}</h1>
+          <p>{task.description}</p>
+          <p className="text-sm text-gray-600">Date</p>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <IconPen className="h-6 w-6" />
+        <IconTrash className="h-6 w-6" />
+      </div>
     </article>
   );
 }

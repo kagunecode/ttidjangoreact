4e345bd1c21bcb2a46a59cd8675db7bf40124227
kagunecode <cesar.acb98@gmail.com>
2024-04-05 12:12:18 -0500

    Added logic for refreshing tasks and id

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -3,19 +3,25 @@ import { instance } from "../utils/axiosInstance";
 import { useParams } from "react-router-dom";
 
 import { TaskCard } from "./TaskCard";
+import { useStore } from "../store";
 
 export function Tasks() {
   const [tasks, setTasks] = useState(null);
+  const [refreshTasks, setRefreshTasks] = useStore((status) => [
+    status.refreshTasks,
+    status.setRefreshTasks,
+  ]);
   const { id } = useParams();
   useEffect(() => {
     (async () => {
       const response = await instance.get(`tasks/${id}`);
       setTasks(response.data);
     })();
-  }, [id]);
+    setRefreshTasks(false);
+  }, [id, refreshTasks]);
 
   return (
-    <div className="w-full flex flex-col gap-2 px-40">
+    <div className="w-full items-center flex flex-col gap-2 px-40">
       {tasks && tasks.map((task) => <TaskCard key={task.id} task={task} />)}
     </div>
   );
